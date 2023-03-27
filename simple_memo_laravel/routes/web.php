6bece6221b2a1aecfb339cd424f184e1c84bc808
TYMtwilight <ore01100001binary@gmail.com>
2023-03-28 06:10:21 +0900

    PHPとLaravelで同じメモアプリを開発しながら、Laravel の魅力を学ぼう！ 5章 : Laravelで作ってみよう（ユーザー登録・認証機能作成） 5-3

--- simple_memo_laravel/routes/web.php
+++ simple_memo_laravel/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 Route::get('/', 'App\Http\Controllers\Auth\LoginController@showLoginForm') -> name('login.index');
+Route::post('/user/register','App\Http\Controllers\Auth\RegisterController@register')->name('user.exec.register');
 Route::get('/user', 'App\Http\Controllers\Auth\RegisterController@showRegistrationForm')->name('user.register');
 Route::get('/memo', function () {
     return view("memo");
